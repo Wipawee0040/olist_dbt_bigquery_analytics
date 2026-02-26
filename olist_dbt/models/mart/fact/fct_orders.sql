cd3d643a263fce6db56ed73eec563855658ab5fa
WER_Wiapwee <benion40710@gmail.com>
2026-02-26 16:16:07 +0000

    fix: aggregation and aliases to resolve duplicates

--- olist_dbt/models/mart/fact/fct_orders.sql
+++ olist_dbt/models/mart/fact/fct_orders.sql
@@ -1,30 +1,30 @@
-WITH orders AS
-(SELECT *
-FROM {{ ref ('stg_orders') }}),
-order_payments AS
-(SELECT 
-    order_id,
-    MAX(payment_type) AS payment_type, 
-        -- รวมยอดเงินทั้งหมดที่จ่ายในออเดอร์นั้น
-    SUM(payment_value) AS payment_value 
-FROM {{ ref('stg_order_payments') }}
-GROUP BY order_id
+WITH orders AS (
+    SELECT * FROM {{ ref('stg_orders') }}
 ),
-order_join AS
-(SELECT
-    o.order_id,
-    o.customer_id,
-    o.status,
-    o.created_at,
-    o.approved_at,
-    o.shipped_at,
-    o.estimated_delivery_at,
-    COALESCE(op.payment_type, 'Other') AS payment_type,
-    COALESCE(MAX(op.payment_value), 0) AS payment_value
-FROM orders o
-    LEFT JOIN order_payments op ON o.order_id = op.order_id
+
+order_payments_summary AS (
+    SELECT 
+        order_id,
+        MAX(payment_type) AS payment_type, 
+        SUM(payment_value) AS payment_value 
+    FROM {{ ref('stg_order_payments') }}
+    GROUP BY 1
+),
+
+order_join AS (
+    SELECT
+        o.order_id,
+        o.customer_id,
+        o.status,
+        o.created_at,
+        o.approved_at,
+        o.shipped_at,
+        o.estimated_delivery_at,
+        COALESCE(p.payment_type, 'Other') AS payment_type,
+        COALESCE(p.payment_value, 0) AS payment_value
+    FROM orders o
+    LEFT JOIN order_payments_summary p 
+    ON o.order_id = p.order_id
 )
 
-SELECT
-*
-FROM order_join
+SELECT * FROM order_join
